d09236b51b8fea21a868c4d6551d3445b372ae99
namakwala1945 <feliciaintl009@gmail.com>
2025-11-24 13:31:11 +0530

    new route.ts add in contact

--- app/contact/ContactClient.tsx
+++ app/contact/ContactClient.tsx
@@ -59,7 +59,7 @@ export default function ContactClient({ pageData }: { pageData: any }) {
     setSending(true);
 
     try {
-      const res = await fetch("/api/contact", {
+      const res = await fetch("/api/send-contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
